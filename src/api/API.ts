399a8dd219a4b66c77052be05c9031728c8debb5
e-nagornyak <dolkiv@gmail.com>
2023-02-18 20:18:45 +0200

    Wrote types for API requests

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+import {newsItem} from "./Types";
 
 const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 })
 
 export const newsAPI = {
-    getNews: (page?: number) => instance.get('/posts', {params: {"_page": page, "_limit": 8}}),
+    getNews: (page?: number) => instance.get<newsItem[]>('/posts', {params: {"_page": page, "_limit": 8}}),
     deleteNews: (id: number) => instance.delete(`/posts/${id}`),
 }
